09e0d41f1614b17e18037008d0714224f2d6b45c
jone <fantas.codes@gmail.com>
2024-09-02 04:45:07 -0700

    compile, test, deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-
-/** @type import('hardhat/config').HardhatUserConfig */
-
-const {vars}=require('hardhat/config');
-
-const INFURA_API_KEY = '1cc8a39be27344e395af4b18f8a5413e';
-
-const SEPOLIA_PRIVATE_KEY = 'ec6fa1d9b31e8b4d22297f4604d7b3c46ca3b4bd7614d7ac9eff1fab9c74ba21';
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
